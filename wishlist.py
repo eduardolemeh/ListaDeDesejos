f53121b40a37c65fa36abd8403d8abab1278ca9d
eduardolemeh <eduardolemeh@gmail.com>
2022-02-02 01:37:24 -0300

    livros, display, grafico e melhoras

--- wishlist.py
+++ wishlist.py
@@ -2,10 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import statistics as stt
+import matplotlib.pyplot as plt
+import random
+from tkinter import *
+from tkinter import messagebox
 
-url = "https://www.amazon.com.br/hz/wishlist/printview/24WHLNWSZI1GN?target=_blank&ref_=lv_pv&filter=unpurchased&sort=default"
+# url
+print('Insira a url da versão para impressão de sua lista de desejos:')
+url = input('')
 resultado = requests.get(url)
-#print(resultado.text)
 doc = BeautifulSoup(resultado.text, "html.parser")
 
 ## valores
@@ -24,9 +29,7 @@ itens = len(lista_p)
 soma = sum(lista_p)
 media = stt.fmean(lista_p)
 
-print('Para comprar todos os {} itens disponíveis de sua lista, com valor médio de {:.2f}, serão necessários R${:.2f}'.format(itens, media, soma))
-
-## autores
+# autores
 autores = doc.find_all(text=re.compile("^de"))
 lista_a = []
 for autor in autores:
@@ -51,7 +54,58 @@ moda = stt.multimode(lista_a)
 moda_str = " e ".join(stt.multimode(lista_a))
 if len(moda) == 1:
     vezes = lista_a.count(moda_str)
-    print('Eis o/a autore mais frequente de sua lista: {} ({}x)'.format(moda_str, vezes))
 elif len(moda) > 1:
     vezes = lista_a.count(moda[1])
-    print('Eis os autores mais frequentes de sua lista: {} ({}x)'.format(moda_str, vezes))
+
+# livros
+livros = doc.find_all(class_="a-text-bold")
+lista_l = []
+for livro in livros:
+    l = str(livro)
+    nl = l[26:-7]
+    lista_l.append(nl)
+qlqr = random.choice(lista_l)
+
+# display
+window = Tk()
+window.withdraw()
+a = str('''Para comprar todos os {} itens disponíveis de sua lista, com valor médio de R${:.2f}, serão necessários R${:.2f}\n
+Eis o/a autore mais frequente de sua lista: {} ({}x)\n
+Por que não dá uma olhada em "{}"?'''.format(itens, media, soma, moda_str, vezes, qlqr))
+messagebox.showinfo('Resultados', a)
+
+# grafico valores
+intervalos = ['<9,9', '10 - 19,9', '20 - 29,9', '30 - 39,9', '40 - 49,9', '50 - 59,9', '60 - 69,9', '70 - 79,9', '>80']
+qntd = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for intervalo in lista_p:
+    i = float(intervalo)
+    if i < 10:
+        qntd[0] += 1
+    elif 10 <= i < 20:
+        qntd[1] += 1
+    elif 20 <= i < 30:
+        qntd[2] += 1
+    elif 30 <= i < 40:
+        qntd[3] += 1
+    elif 40 <= i < 50:
+        qntd[4] += 1
+    elif 50 <= i < 60:
+        qntd[5] += 1
+    elif 60 <= i < 70:
+        qntd[6] += 1
+    elif 70 <= i < 80:
+        qntd[7] += 1
+    elif i > 80:
+        qntd[8] += 1
+
+plt.figure(figsize=(8.0, 6.0))
+plt.yticks(fontsize=9)
+plt.xticks(fontsize=8)
+plt.title('Valores')
+plt.ylabel('Quantidade')
+plt.xlabel('Valor', color='blue')
+plt.axis(ymin=0, ymax=70)
+plt.bar(intervalos, qntd, width=0.65)
+
+plt.show()
